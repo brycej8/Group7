2d266c67377d583b344d784071fa686ddae8ed0a
IsaiahKindred <isaiahekindred@gmail.com>
2025-02-25 21:47:03 -0500

    Changed the Login.php to the one on the server

--- API/Login.php
+++ API/Login.php
@@ -1,59 +1,57 @@
 <?php
-require 'connSetup.php';
-session_start(); // Start a session
 
-// Enable CORS headers to allow requests from any origin
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+	$inData = getRequestInfo();
+	
+	$id = 0;
+	$firstName = "";
+	$lastName = "";
 
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    exit(0);
-}
+	$conn = new mysqli("3.217.131.95", "root", "!T!Wwzckon7m", "group7"); 	
+	if( $conn->connect_error )
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		$stmt = $conn->prepare("SELECT ID,firstName,lastName FROM Users WHERE Login=? AND Password =?");
+		$stmt->bind_param("ss", $inData["login"], $inData["password"]);
+		$stmt->execute();
+		$result = $stmt->get_result();
 
-$inData = getRequestInfo();
+		if( $row = $result->fetch_assoc()  )
+		{
+			returnWithInfo( $row['firstName'], $row['lastName'], $row['ID'] );
+		}
+		else
+		{
+			returnWithError("No Records Found");
+		}
 
-if (!isset($inData["login"]) || !isset($inData["password"])) {
-    returnWithError("Missing login or password.");
-    exit();
-}
+		$stmt->close();
+		$conn->close();
+	}
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
 
-$login = $inData["login"];
-$password = $inData["password"];
-
-$stmt = $conn->prepare("SELECT ID, firstName, lastName FROM Users WHERE Login=? AND Password=?");
-$stmt->bind_param("ss", $login, $password);
-$stmt->execute();
-$result = $stmt->get_result();
-
-if ($row = $result->fetch_assoc()) {
-    $_SESSION['userID'] = $row['ID']; // Store user ID in session
-    $_SESSION['firstName'] = $row['firstName'];
-    $_SESSION['lastName'] = $row['lastName'];
-    
-    returnWithInfo($row['firstName'], $row['lastName'], $row['ID']);
-} else {
-    returnWithError("Incorrect username or password.");
-}
-
-$stmt->close();
-$conn->close();
-
-function getRequestInfo() {
-    return json_decode(file_get_contents('php://input'), true);
-}
-
-function returnWithError($err) {
-    sendResultInfoAsJson(["id" => 0, "firstName" => "", "lastName" => "", "error" => $err]);
-}
-
-function returnWithInfo($firstName, $lastName, $id) {
-    sendResultInfoAsJson(["id" => $id, "firstName" => $firstName, "lastName" => $lastName, "error" => ""]);
-}
-
-function sendResultInfoAsJson($obj) {
-    header('Content-type: application/json');
-    echo json_encode($obj);
-    exit();
-}
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $firstName, $lastName, $id )
+	{
+		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
 ?>
